bcf2af8ea07ceb3022e7c9ddb33172cb39c91b14
Sean McLaughlin <mclaughlin6464@gmail.com>
2018-04-12 15:21:09 -0700

    Wrote a setup.py too, seemed sensible.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name = 'Aemulator',
+      version = '0.1',
+      description= 'Abstract base class for Aemulus Project emulators.',
+      author = 'Sean McLaughlin',
+      author_email = 'swmclau2@stanford.edu',
+      url = 'https://github.com/AemulusProject/Aemulator',
+      packages=['Aemulator'])
